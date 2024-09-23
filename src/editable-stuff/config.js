3989ded16c3ae438c231b2d3a8467b4fb7bab9e2
Jamie Waters <judoguy12@protonmail.com>
2024-09-24 00:53:49 +0100

    Changes to package

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,30 +6,22 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "Jamie",
   middleName: "",
-  lastName: "Shoaib",
-  message: " Passionate about changing the world with technology. ",
+  lastName: "Waters",
+  message: " Passionate about enabling inovative thought provoking theatre.  ",
   icons: [
     {
-      image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
-    },
-    {
-      image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
-    },
-    {
       image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
+      url: "https://www.instagram.com/housane12/",
     },
     {
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
+      url: "https://www.linkedin.com/in/amie-waters-450a021b5/",
     },
     {
-      image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
+      image: "fa-facebook",
+      url: "https://www.facebook.com/share/YCuzcHP9ZZG4Tqss/?mibextid=LQQJ4d",
     },
   ],
 };
@@ -51,11 +43,11 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  profiePictureLink: "housane12",
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
-  resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+    "My name is Jamie and I am a Freelance AV and Theatre Technician with a focus on live sound. I have 8 years of technical experience, including a year of professional work. I appreciate the creative problem solving involved in technical theatre and especially theatrical sound. I have worked on a range of shows and events from supervising sound at Ed fringe 2024 to a small-scale tour with NTW in late 2023. I am passionate about how immersive and semi-immersive theatre can involve the audience in experiencing a deeper connection to the story.",
+  resume: "https://nextcloud.housane.co.uk/s/H8LZBByGem4NS43",
 };
 
 // PROJECTS SECTION
@@ -64,29 +56,29 @@ const about = {
 // If you want to display specfic projects, add the repository names,
 //      i.e ["repository-1", "repo-2"]
 const repos = {
-  show: true,
+  show: false,
   heading: "Recent Projects",
   gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
-  reposLength: 4,
+  reposLength: 0,
   specificRepos: [],
 };
 
 // Leadership SECTION
 const leadership = {
-  show: false,
-  heading: "Leadership",
+  show: true,
+  heading: "Recent Projects",
   message:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+    "Here are a selection of projects I have recently worked on",
   images: [
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "First slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+      img: require("../editable-stuff/fringe.png"), 
+      label: "Sound Supervisor - Edinbrugh Fringe 2024", 
+      paragraph: "Looking after the medium sized venues from a Sound/Video perspective throughout the festival proividing first line support to the venue crews to ensure smooth running of the festival, based at the Pleasance Courtyard." 
     },
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "Second slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+      img: require("../editable-stuff/cof.png"), 
+      label: "Sound & Video Technician - Circle of Fifths", 
+      paragraph: "This was a small scale Welsh tour exploring the rituals around death and dying in the Butetown community. I was responsible for all of the Sound and Video aspects of the show while on tour, as well as responsible for Sound through the rehearsal process." 
     },
   ],
   imageSize: {
@@ -97,28 +89,14 @@ const leadership = {
 
 // SKILLS SECTION
 const skills = {
-  show: true,
+  show: false,
   heading: "Skills",
   hardSkills: [
-    { name: "Python", value: 90 },
-    { name: "SQL", value: 75 },
-    { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
-  ],
-  softSkills: [
-    { name: "Goal-Oriented", value: 80 },
-    { name: "Collaboration", value: 90 },
-    { name: "Positivity", value: 75 },
-    { name: "Adaptability", value: 85 },
-    { name: "Problem Solving", value: 75 },
-    { name: "Empathy", value: 90 },
-    { name: "Organization", value: 70 },
-    { name: "Creativity", value: 90 },
-  ],
+    { name: "Q-Lab", value: 80 },
+    { name: "ETC Eos", value: 65 },
+    { name: "dlive", value: 70 },
+    { name: "Project Management", value: 70 },
+  ]
 };
 
 // GET IN TOUCH SECTION
@@ -126,23 +104,33 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+    "I'm currently looking for freelance oppotunities in and around London, if you think theres a project we could work together on please reach out!",
+  email: "hello@jrwtechnical.uk",
 };
 
 const experiences = {
-  show: false,
-  heading: "Experiences",
+  show: true,
+  heading: "Skills",
   data: [
     {
-      role: 'Software Engineer',// Here Add Company Name
-      companylogo: require('../assets/img/dell.png'),
-      date: 'June 2018 – Present',
+      role: 'Dante Networking',// Here Add Company Name
+      date: 'Acheved June 2024',
     },
     {
-      role: 'Front-End Developer',
-      companylogo: require('../assets/img/boeing.png'),
-      date: 'May 2017 – May 2018',
+      role: 'First Aid at Work',
+      date: 'Valid to November 2024',
+    },
+    {
+      role: 'Q-Lab',
+      date: 'Solid understanding of Q-Lab networking, sound and video',
+    },
+    {
+      role: 'Desks I have worked on',
+      date: 'X32, dLive, Yamaha CL/QL, DiGiCo SD10, SQ series',
+    },
+    {
+      role: 'Project Management',
+      date: 'Experience accros a variety of projects from live music to musicals to theatre shows with both large and small teams',
     },
   ]
 }
