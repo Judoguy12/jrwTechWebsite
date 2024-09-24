b57ad9baac7da803932229a1b7ddd44e75198dbc
Jamie Waters <judoguy12@protonmail.com>
2024-09-24 03:09:45 +0100

    add privacy policy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,6 +23,8 @@ const Footer = (props) => {
             Project code is open source. Feel free to fork and make your own
             version.
           </small>
+          <small className="text-muted"><a href="https://drive.proton.me/urls/S03HJFEKF8#oi0YcXXqT3HW" target="_blank">Privacy Policy</a>
+</small>
         </p>
       </Container>
     </footer>
