e6c77e1d68b3eac531a82672d4608c4ee770d7e3
Jamie Waters <judoguy12@protonmail.com>
2024-09-24 02:16:09 +0100

    Updated carosel

--- src/components/home/Leadership.jsx
+++ src/components/home/Leadership.jsx
@@ -11,16 +11,16 @@ const Leadership = ({ heading, message, img, imageSize }) => {
     >
       <h2 className="display-3 fw-bold pb-5 text-center">{heading}</h2>
       <div className="row">
-        <div className="md-5 text-center display-3">
+        <div className="text-center pb-5 display-3">
           <p className="lead">{message}</p>
         </div>
-        <div className="md-5">
+        <div className="p-2">
           <Carousel>
             {img.map((value, index) => {
               return (
                 <Carousel.Item key={index}>
                   <img
-                    className="d-block w-200"
+                    className="d-block w-100"
                     src={value.img}
                     alt="First slide"
                     width={imageSize.width}
