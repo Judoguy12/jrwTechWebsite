b0d75c810c2350edd0066a9c5d81cc4818e1b48f
Jamie Waters <judoguy12@protonmail.com>
2024-09-24 02:46:26 +0100

    Fix navbar

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -132,4 +132,4 @@ const experiences = {
 //   show: false,
 // };
 
-export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
+export { navBar, mainBody, about, skills, leadership, getInTouch, experiences };
