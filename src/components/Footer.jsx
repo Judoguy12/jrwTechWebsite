7d4027314395c10cb9f8d4ea555bdc516bf4659c
Jamie Waters <judoguy12@protonmail.com>
2024-09-24 01:22:46 +0100

    Changed font weights

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = (props) => {
     <footer style={bgStyle} className="mt-auto py-5 text-center ">
       <Container>
         {props.children}
-        <i className="fas fa-code" /> with <i className="fas fa-heart" /> by{" "}
+         <i className="fas fa-code" /> with <i className="fas fa-heart" /> by{" "}
         <a
           rel="noopener"
-          href="https://github.com/hashirshoaeb"
+          href="https://github.com/judoguy12"
           aria-label="My GitHub"
         > <span className="badge bg-dark">
-            Hashir Shoaib
+            Jamie Waters
           </span>
         </a>{" "}
         using <i className="fab fa-react" />
--- src/components/home/Experience.jsx
+++ src/components/home/Experience.jsx
@@ -11,7 +11,7 @@ const Experience = ({ experiences }) => {
     <section className="section">
       <Container>
         <Jumbotron className="bg-white">
-          <h2 className="display-4 mb-5 text-center">
+          <h2 className="display-2 fw-bold mb-5 text-center">
             {experiences.heading}
           </h2>
           <Row>
